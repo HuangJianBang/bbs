ba418b007b9923659a9eb656e17a43d0de635043
sshpark <huangjiaming@gmail.com>
2018-06-08 19:43:59 +0800

    添加获取登陆用户Id的方法

--- src/main/java/bbs_gradle/bbs/controller/MainController.java
+++ src/main/java/bbs_gradle/bbs/controller/MainController.java
@@ -1,11 +1,20 @@
 package bbs_gradle.bbs.controller;
 
+import bbs_gradle.bbs.dao.UserRepository;
 import bbs_gradle.bbs.model.User;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/")
     public String index() {
         return "index";
@@ -20,4 +29,15 @@ public class MainController {
     public String register(User user) {
         return "/users/register";
     }
+
+    /**
+     * @return 当前登陆用户的Id
+     */
+    private Long getCurrentUserId() {
+        UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        String username = userDetails.getUsername();
+        return userRepository.findByUsername(username).getId();
+    }
+    
 }
